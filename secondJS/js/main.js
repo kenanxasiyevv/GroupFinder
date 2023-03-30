70836a1495e9d06e07f3a714efc029a4742f3efc
Kanan Xasiyev <121182410+kenanxasiyevv@users.noreply.github.com>
2023-03-30 19:08:43 +0400

    Add files via upload

---
+++ secondJS/js/main.js
@@ -0,0 +1,27 @@
+var buton = document.getElementById('btn');
+
+buton.onclick = function () {
+    var color = document.getElementById('bgcolor');
+    var input = document.getElementById('inp').value;
+    var text = document.getElementById('text');
+    var num = Number(input.slice(-3, -2));
+
+    if (num == 3) {
+        color.style.backgroundImage = "url(https://img.freepik.com/free-vector/full-moon-night-ocean-cartoon-illustration_33099-2308.jpg?w=2000)";
+        text.innerHTML = "Axşam Qrupudur!"
+    }
+
+    else if (num == 2) {
+        color.style.backgroundImage = "url(https://wallpaperaccess.com/full/4349823.jpg)";
+        text.innerHTML = "Günorta Qrupudur!"
+    }
+
+    else if (num == 1) {
+        color.style.backgroundImage = "url(https://static.vecteezy.com/system/resources/previews/004/482/791/original/sunrise-in-mountains-nature-landscape-background-the-sun-shines-behind-the-mountains-with-the-cool-morning-air-under-the-dawn-sky-morning-view-cartoon-illustration-vector.jpg)";
+        text.innerHTML = "Səhər Qrupudur!"
+    }
+
+    else {
+        text.innerHTML = "Qrup düzgün daxil edilməyib!"
+    }
+}
