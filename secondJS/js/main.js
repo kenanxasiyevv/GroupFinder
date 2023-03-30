561894dd8f3cc926c6a46a2b29b1c827b05c6a46
Kanan Xasiyev <121182410+kenanxasiyevv@users.noreply.github.com>
2023-03-30 20:55:46 +0400

    Add files via upload

--- secondJS/js/main.js
+++ secondJS/js/main.js
@@ -9,16 +9,19 @@ buton.onclick = function () {
     if (num == 3) {
         color.style.backgroundImage = "url(https://img.freepik.com/free-vector/full-moon-night-ocean-cartoon-illustration_33099-2308.jpg?w=2000)";
         text.innerHTML = "Axşam Qrupudur!"
+         text.style.color='black'
     }
 
     else if (num == 2) {
         color.style.backgroundImage = "url(https://wallpaperaccess.com/full/4349823.jpg)";
         text.innerHTML = "Günorta Qrupudur!"
+        text.style.color='black'
     }
 
     else if (num == 1) {
         color.style.backgroundImage = "url(https://static.vecteezy.com/system/resources/previews/004/482/791/original/sunrise-in-mountains-nature-landscape-background-the-sun-shines-behind-the-mountains-with-the-cool-morning-air-under-the-dawn-sky-morning-view-cartoon-illustration-vector.jpg)";
         text.innerHTML = "Səhər Qrupudur!"
+        text.style.color='white'
     }
 
     else {
